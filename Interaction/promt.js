eef7ca414eb7a3467ef55fb6c28bf06f0be0ed78
Madhu2704 <beharamadhu2704@gmail.com>
2020-08-23 06:31:55 +0530

    Interaction methods in JS

---
+++ Interaction/promt.js
@@ -0,0 +1,10 @@
+// The function prompt accepts two arguments:
+
+//prompt('STATEMENT MSG','default value of the varible')
+let result = prompt('title', 'SAAHO');
+alert(`YOU TITLE IS ${result}`)
+// It shows a modal window with a text message, an input field for the visitor, and the buttons OK/Cancel.
+// title
+// The text to show the visitor.
+// default
+// An optional second parameter, the initial value for the input field.
